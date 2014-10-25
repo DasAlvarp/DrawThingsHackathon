a9d1cb0b2ad6368d782241ef57f10fbeda5b25ec
alvarq <alvarogudis@gmail.com>
2014-10-25 14:00:58 -0500

    Made things work.

--- app/src/main/java/com/jaks/alvarp/drawthing/DrawScreen.java
+++ app/src/main/java/com/jaks/alvarp/drawthing/DrawScreen.java
@@ -1,18 +1,54 @@
 package com.jaks.alvarp.drawthing;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 
-public class DrawScreen extends Activity {
+public class DrawScreen extends Activity
+{
+    private DrawingView drawView;
+    private ImageButton currPaint;
+
+
+
+
+
+
+
+    public void paintClicked(View view){
+        if(view!=currPaint)
+        {
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
+
+
+
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_screen);
+        drawView = (DrawingView)findViewById(R.id.drawing);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
     }
+
 
 
     @Override
--- app/src/main/java/com/jaks/alvarp/drawthing/DrawingView.java
+++ app/src/main/java/com/jaks/alvarp/drawthing/DrawingView.java
@@ -1,5 +1,6 @@
 package com.jaks.alvarp.drawthing;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.graphics.Bitmap;
@@ -7,11 +8,25 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.view.MotionEvent;
+import android.widget.ImageButton;
+
 /**
  * Created by alvaro on 10/25/14.
  */
 public class DrawingView extends View
 {
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //initial color
+    private int paintColor = 0xFF660000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+
+
     public DrawingView(Context context) {
         super(context);
     }
@@ -20,7 +35,68 @@ public class DrawingView extends View
         super(context, attrs);
         setupDrawing();
     }
-    private void setupDrawing(){
+
+
+    public void setColor(String newColor)
+    {
+        invalidate();
+        paintColor = Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+
+    }
+
+    private void setupDrawing()
+    {
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        drawPaint.setColor(paintColor);
+
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
         //get drawing area setup for interaction
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
     }
 }
